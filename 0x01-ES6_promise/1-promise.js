94ee9c20b8efac60e869158e1ec0e14268831dae
Arsema-Zeweldi <arsugeb@gmail.com>
2022-12-21 23:37:50 +0300

    fix task 1

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -3,9 +3,9 @@ export default function getFullResponseFromAPI(success) {
     if (success) {
       resolve({
         status: 200,
-        body: 'Success'
-      })
+        body: 'Success',
+      });
     }
-    reject(new Error('The fake API is not working currently'))
-  })
+    reject(new Error('The fake API is not working currently'));
+  });
 }
