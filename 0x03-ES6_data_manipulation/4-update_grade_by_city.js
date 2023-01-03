53c14257c2b2920d2ed8a65f3e2b76afb44a50db
Arsema-Zeweldi <arsugeb@gmail.com>
2023-01-03 18:52:21 +0300

    fix 4

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -2,7 +2,7 @@ export default function updateStudentGradeByCity(list, city, newGrades) {
   const mapList = list.map((student) => {
     const grade = newGrades.filter((key) => key.studentId === student.id)
       .map((value) => value.grade)[0];
-    student['grade'] = grade || 'N/A';
+    student.grade = grade || 'N/A';
     return student;
   });
   return mapList.filter((key) => key.location === city);
